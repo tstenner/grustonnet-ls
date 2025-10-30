940338d5364432b2036e2a376f25cba84d9fcecf
Kevin Köster <kevin.koester@ppi.de>
2025-10-30 09:16:02 +0100

    fix(rust2go): Properly set the GODEBUG variable to prevent weird go crashes

--- crates/grustonnet-bin/src/main.rs
+++ crates/grustonnet-bin/src/main.rs
@@ -16,12 +16,23 @@ struct Args {
 
 #[tokio::main]
 async fn main() {
-    // At this point we are single threaded. Therefore this is safe
-    unsafe {
-        // Go seems to scan the stack an will panic upon encountering a 0x1 pointer.
-        // However, Rust does use this value in some cases
-        // If this turns out to be a problem we'll need to switch to an ipc based solution
-        std::env::set_var("GODEBUG", "invalidptr=0,cgocheck=0");
+    // Go seems to scan the stack an will panic upon encountering a 0x1 pointer.
+    // However, Rust does use this value in some cases
+    // Just setting the variable is not enough. Therefore we'll set the environment
+    // variable and restart the current program
+    // If this turns out to be a problem we'll need to switch to an ipc based solution
+    if std::env::var("GODEBUG").is_err() {
+        // At this point we are single threaded. Therefore this is safe
+        unsafe {
+            std::env::set_var("GODEBUG", "invalidptr=0,cgocheck=0");
+        }
+
+        let exe = std::env::current_exe().expect("Could not get path to the current executable");
+        let args: Vec<String> = std::env::args().collect();
+
+        let err = exec::execvp(&exe, &args);
+        eprintln!("Failed to restart with GODEBUG: {}", err);
+        std::process::exit(1);
     }
 
     #[cfg(feature = "tracing")]
