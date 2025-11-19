2506a03cf838a1ddfc77e62aebd5d5a857c5c92b
Kevin Köster <kevin.koester@ppi.de>
2025-11-19 13:57:46 +0100

    fix(uri): Fallback to from_str if the path cannot be found

--- crates/language-server/src/utils/mod.rs
+++ crates/language-server/src/utils/mod.rs
@@ -15,8 +15,9 @@ pub trait UriHelper {
 impl UriHelper for Uri {
     fn from_path<P: AsRef<Path>>(val: P) -> Result<Uri> {
         let mut orig_uri = Uri::from_str(val.as_ref().to_str().ok_or(anyhow!("Invalid path"))?)?;
-        if orig_uri.scheme().is_none() {
-            let absolute = fs::canonicalize(val)?;
+        if orig_uri.scheme().is_none()
+            && let Ok(absolute) = fs::canonicalize(val)
+        {
             orig_uri = Uri::from_str(&format!(
                 "file://{}",
                 absolute
