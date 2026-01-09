e29401aa1d6378d72a3b7b9aaf5854e379566942
Kevin Köster <kevin.koester@ppi.de>
2026-01-09 16:05:31 +0100

    fix(diagnostics): Disable the object_function linter by default to prevent endless recursion

--- crates/grustonnet-config/src/lib.rs
+++ crates/grustonnet-config/src/lib.rs
@@ -116,7 +116,7 @@ pub struct DiagnosticConfig {
     /// Throws an error if a top level function is missing a default argument
     pub top_level_function_args: bool,
 
-    #[default = true]
+    #[default = false]
     /// Throws an error if the body of an object field is a function but is not hidden
     pub object_function: bool,
 }
